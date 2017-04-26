3ea31242bcf37fcd1dd5f7008a3f503902489e8a
Matt Larson <matt.a.larson@gmail.com>
2017-04-26 12:55:34 -0500

    got DB connected

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,16 +1,23 @@
-$( document ).ready( function(){
-  console.log( 'JQ' );
+$( document ).ready( onReady );
+
+function onReady() {
+  console.log('onReady');
   getCars();
-});
+} // end onReady
 
 function getCars(){
   // get call to server for cars
   $.ajax({
     url: '/cars',
-    type:'GET',
+    type: 'GET',
     success: function( response ){
       console.log( 'back from server with:', response );
+      $('.container').empty();
+      for (var i = 0; i < response.length; i++) {
+        $('.container').append('<p>' + response.year + response.make + response.model + '</p>');
+      }
+      // receive cars array
       // update dom
     } // end success
-  }) // end ajax
+  }); // end ajax
 } // end getCars
--- server.js
+++ server.js
@@ -3,6 +3,17 @@ var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
+var pg = require( 'pg' );
+
+// setup config for the pool
+var config = {
+  database: 'My Garage',
+  host: 'localhost',
+  port: 5432,
+  max: 12
+};
+
+var pool = new pg.Pool( config );
 
 // uses
 app.use( bodyParser.urlencoded( { extended: true } ) );
@@ -19,15 +30,33 @@ app.get( '/', function ( req, res ){
 
 app.get( '/cars', function( req, res ){
   console.log( 'in /cars get request' );
-  // temp array
-  var cars = [ 'car1', 'car2', 'car3' ];
-  // connect to DB
-  // send query to DB that return all cars
+  // empty array of cars to push to
+  var allCars = [];
   // translate the resultSet of query to the "cars" array
   // after the last row has been pushed send back the object with array
-  var objectToSend={
-    status: 200,
-    allCars: cars
-  }; // end objectToSend
-  res.send( objectToSend );
+
+  // connect to DB
+  pool.connect( function( err, connection, done ) {
+    // check for error
+    if( err ){
+      console.log( err );
+      res.sendStatus( 400 );
+    } // end Error
+    else{
+      console.log( 'connected to DB' );
+      // send query to DB that return all cars in the table and hold in a var resultSet
+      var resultSet = connection.query( "SELECT * FROM cars" );
+      // convert each row into an object in the allCars array
+      // on each row, push the row into allCars
+      resultSet.on( 'row', function( row ) {
+        allCars.push( row );
+      }); // end on row
+      resultSet.on( 'end', function(){
+        // close connection to reopen spot in pool
+        done(); // run done before res.send
+        // res.send array of cars
+        res.send( allCars );
+      }); // end on end
+    } // end NO ERR
+  }); // end pool
 }); //end cars get
